44ac9068c72610dc5249c0be3b7b715b53a53935
CLe4N <64629943+CLe4N@users.noreply.github.com>
2020-11-22 05:46:00 +0700

    Update Move.cs

--- Move.cs
+++ Move.cs
@@ -8,22 +8,21 @@ public class Move : MonoBehaviour
     Rigidbody rb;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); // access Rigidbody component
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float Xais = Input.GetAxisRaw("Horizontal");
-        float Zaix = Input.GetAxisRaw("Vertical");
+        float Xais = Input.GetAxisRaw("Horizontal"); // get axis input "Horizontal"
+        float Zaix = Input.GetAxisRaw("Vertical"); // get axis input "Vertical"
 
-        if(Xais != 0 || Zaix != 0)
+        if(Xais != 0 || Zaix != 0) // if Xais is not 0 or Zaix is not 0
         {
-            rb.velocity = new Vector3(Xais * speed,rb.velocity.y,Zaix * speed);
+            rb.velocity = new Vector3(Xais * speed,rb.velocity.y,Zaix * speed); // this gameobject velocity = axis input
         }
         else
         {
-            rb.velocity = new Vector3(0, rb.velocity.y, 0);
+            rb.velocity = new Vector3(0, rb.velocity.y, 0); = velocity = 0
         }
     }
 }
